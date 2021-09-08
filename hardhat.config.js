8f1441a7ead7ba97aeb535111f01631c550d8a64
ratik <ratikjindal21@gmail.com>
2021-09-09 03:51:21 +0530

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,8 +17,36 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    binance: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: ['69ca251a19064f103500412822a14b73ff73d4de4e2ca310bd9e1b599dbbb53c',
+      'e75c8e43573aa5a64452a741fbcdc424ffc83dc66af04520906d04ceef8dc24a',
+      'd07fa9bdb1c7abe353ae51681792d328a4749afee918dbdee58d61c715807d1c',
+      'f82106aeea22687c1414fe6b237a1941d58130b2d57bc39b32a2873a78d79fc9']
+    },
+    avalanche: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      gas: 3100000,
+      gasPrice: 76000000000,
+      accounts: ['69ca251a19064f103500412822a14b73ff73d4de4e2ca310bd9e1b599dbbb53c',
+      'e75c8e43573aa5a64452a741fbcdc424ffc83dc66af04520906d04ceef8dc24a',
+      'd07fa9bdb1c7abe353ae51681792d328a4749afee918dbdee58d61c715807d1c',
+      'f82106aeea22687c1414fe6b237a1941d58130b2d57bc39b32a2873a78d79fc9']
+    },
+    hardhat: {
+    },
+    rinkeby: {
+      url: "https://ropsten.infura.io/v3/d5b63801f95e473db76a3ca98a7930f9",
+      accounts: ['69ca251a19064f103500412822a14b73ff73d4de4e2ca310bd9e1b599dbbb53c']
+    }
+  },
   solidity: {
     version: "0.8.0",
+    defaultNetwork: "avalanche",
     settings:{
       optimizer: {
         enabled: true,
@@ -27,5 +55,5 @@ module.exports = {
       evmVersion: "istanbul"
     }
   },
-  
+
 };
