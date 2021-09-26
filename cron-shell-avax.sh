d42468becee5957d3ddaa8d8aaad375d3326b5c3
ratik <ratikjindal21@gmail.com>
2021-09-27 00:23:45 +0530

    rename cron-shell to harmony

--- cron-shell-avax.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# setup command(for every 5min) in "crontab -e": */5 * * * * sh /tmp/Prediction-market-contracts/cron-shell.sh
-. $HOME/.bashrc
-cd /tmp/Prediction-market-contracts
-npx hardhat run scripts/cron-single.js --network avalanche
